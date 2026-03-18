233bf0fe8358b7a42f2b2a10be6680cca4dc3520
sanaansari574 <sanajamal2004@gmail.com>
2026-03-18 15:56:55 +0500

    passing test: deleteCity

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -20,4 +20,11 @@ public class CustomList {
     public boolean hasCity(City city) {
         return cities.contains(city);
     }
+
+    public void deleteCity(City city){
+        if(!cities.contains(city)){
+            throw new IllegalArgumentException("City does not exist");
+        }
+        cities.remove(city);
+    }
 }
